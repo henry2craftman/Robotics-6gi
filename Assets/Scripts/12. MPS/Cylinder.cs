b0130255e0e4ff39c2614a85941cb42f04e9492b
danric-cho <seungjuy192@gmail.com>
2025-09-11 17:36:34 +0900

    0911수업
    실린더 제어를 위한 유니티

--- Assets/Scripts/12. MPS/Cylinder.cs
+++ Assets/Scripts/12. MPS/Cylinder.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 // 목표: 실린더의 Rod를 minRange에서 maxRange로 특정 속도로 이동시킨다.
 // 속성: Rod의 transform, minRange, maxRange, 속도
@@ -11,6 +12,19 @@ public class Cylinder : MonoBehaviour
     public float MinRange { get => minRange; set => value = minRange; }
     [SerializeField] float maxRange;
     [SerializeField] float speed;
+    [SerializeField] Renderer forwardLS;
+    [SerializeField] Renderer backwardLS;
+    Color OriginLSColor;
+    [SerializeField] bool isForwardSWON = false;
+    [SerializeField] bool isBackSWON = true;
+    bool isForwarding = false;
+
+
+    private void Start()
+    {
+        OriginLSColor = forwardLS.material.color;
+        backwardLS.material.color = Color.green;
+    }
 
     // CylinderForward 버튼을 누르면 실린더가 전진한다.
     public void OnCylinderForwardEvent()
@@ -18,7 +32,12 @@ public class Cylinder : MonoBehaviour
         Vector3 startPos = new Vector3(0, minRange, 0);
         Vector3 endPos = new Vector3(0, maxRange, 0);
 
+        isForwardSWON = false;
+        isForwarding = true;
+
         StartCoroutine(CoMoveCylinder(startPos, endPos));
+
+        backwardLS.material.color = OriginLSColor;
     }
 
     IEnumerator CoMoveCylinder(Vector3 from, Vector3 to)
@@ -31,6 +50,22 @@ public class Cylinder : MonoBehaviour
 
             if (distance < 0.1f)
             {
+
+                if (isForwarding) //전진신호를 받았을때
+                {
+                    isForwardSWON = true;
+
+                    forwardLS.material.color = Color.green;
+                }
+                else if(!isForwarding)//후진신호를 받았을때
+                {
+                    isBackSWON = true;
+
+                    backwardLS.material.color = Color.green;
+                }
+
+                    isForwardSWON = !isForwardSWON;
+                isBackSWON  = !isBackSWON;
                 break;
             }
 
@@ -38,5 +73,17 @@ public class Cylinder : MonoBehaviour
 
             yield return new WaitForEndOfFrame();
         }
+    }
+
+    public void OnCylinderBackwardEvent() 
+    {
+        Vector3 startPos = new Vector3(0, minRange, 0);
+        Vector3 endPos = new Vector3(0, maxRange, 0);
+
+        isForwardSWON = false;
+        isForwarding = false;
+        StartCoroutine(CoMoveCylinder(endPos, startPos));
+
+        forwardLS.material.color = OriginLSColor;
     }
 }
