42e8a0cb0f3c4ab0ec6dcb22ca32e893618a27c4
henry2craftman <henry2craftman@gmail.com>
2025-09-12 16:20:13 +0900

    add Loader 스크립트 추가

---
+++ Assets/Scripts/12. MPS/Loader.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    [SerializeField] GameObject[] objPrefabs;
+    public bool isLoadedSignal = false;
+
+    public void OnLoadBtnClkEvent()
+    {
+        int rand = Random.Range(0, objPrefabs.Length);
+
+        GameObject obj = Instantiate(objPrefabs[rand]);
+
+        obj.transform.position = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Metal" || other.tag == "Plastic")
+        {
+            isLoadedSignal = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Metal" || other.tag == "Plastic")
+        {
+            isLoadedSignal = false;
+        }
+    }
+}
