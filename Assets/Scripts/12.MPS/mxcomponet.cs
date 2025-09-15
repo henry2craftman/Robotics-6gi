5efde3e69529d68d1eb484b2ba863085c3429084
Sukiicat-creator <dh777es@gmail.com>
2025-09-15 17:45:43 +0900

    250915 update 최종
    update

--- Assets/Scripts/12.MPS/mxcomponet.cs
+++ Assets/Scripts/12.MPS/mxcomponet.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using ActUtlType64Lib;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 
 // 목표 : UI(연결, 연결해제) 버튼을 누르면 실시간으로 PLC에 데이터를 요청하고 쓴다.
@@ -12,14 +14,25 @@ using System;
 public class mxcomponet : MonoBehaviour
 {
     ActUtlType64 mxComponent;
+
+    [Header("PLC 정보")]
     public bool isConnected = false;
-    public float updateInterval = 1;
+    public List<bool[]> yOutputs;
+    public float updateInterval = 1f;
+    public string xInputStartDevice = "X0";
+    public int xInputBlockCount = 1;
+    public string yOutputStartDevice = "Y0";
+    public int yOutputBlockCount = 1;
+
+    [Header("가상 설비 참조")]
+    public List<Cylinder> cylinders;
 
 
     // Lifecycle 함수 중 가장 빨리 실행.
     private void Awake()
     {
         mxComponent = new ActUtlType64();
+        mxComponent.ActLogicalStationNumber = 0; // default : 0
     }
 
     public void OnOpenBtnClkEvent()
@@ -36,6 +49,8 @@ public class mxcomponet : MonoBehaviour
         else
         {
             isConnected = true;
+
+            StartCoroutine(CoUpdatePLCData());
 
             Debug.Log("PLC가 연결되었습니다.");
         }
@@ -68,15 +83,82 @@ public class mxcomponet : MonoBehaviour
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    IEnumerator CoUpdatePLCData()
     {
-        
+        while(true)
+        {
+            ReadDeviceBlock(xInputStartDevice, xInputBlockCount);
+
+            yield return new WaitForSeconds(updateInterval);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReadDeviceBlock(string _xInputStartDevice, int _xInputBlockCount)
     {
-        
+        // 10진수 -> 2진수 변환
+        int[] data = new int[_xInputBlockCount];
+        int iRet = mxComponent.ReadDeviceBlock(_xInputStartDevice, _xInputBlockCount, out data[0]);
+
+        CheckError(iRet);
+
+        List<bool[]> yOutputs = new List<bool[]>();
+
+        for(int i =0; i < data.Length; i++)
+        {
+            bool[] block = new bool[16];
+
+            yOutputs.Add(block);
+        }
+
+        yOutputs = ConvertDecimalToBinary(data);
+
+        // 각 GameObject의 스크립트에 연결
+        cylinders[0].isForwardSignal = yOutputs[0][0];
+        cylinders[0].isBackwardSignal = yOutputs[0][1];
+        cylinders[1].isForwardSignal = yOutputs[0][2];
+        cylinders[1].isBackwardSignal = yOutputs[0][3];
+        cylinders[2].isForwardSignal = yOutputs[0][4];
+        cylinders[2].isBackwardSignal = yOutputs[0][5];
+        cylinders[3].isForwardSignal = yOutputs[0][6];
+        cylinders[3].isBackwardSignal = yOutputs[0][7];
     }
+
+    // 10진수 -> 2진수 bool 배열로 변환하는 메서드
+    private List<bool[]> ConvertDecimalToBinary(int[] data)
+    {
+        List<bool[]> result = new List<bool[]>();
+
+        for(int i = 0; i < data.Length; i++)
+        {
+            bool[] block = new bool[16];
+
+            for(int j = 0; j < block.Length; j++)
+            {
+                bool isBitSet = ((data[i] & (1 << j)) != 0);
+                block[j] = isBitSet;
+            }
+
+            result.Add(block);
+        }
+
+        return result;
+    }
+
+    private static void CheckError(int iRet)
+    {
+        if (iRet != 0)
+        {
+            string error = Convert.ToString(iRet, 16);
+            Debug.LogError(error);
+
+            return;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        OnCloseBtnClkEvent();
+    }
+
+
 }
